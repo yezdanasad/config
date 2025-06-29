f245c3400bbf02d5049c300b65219408919bd5a2
Asad Yezdan <yezdanasad@gmail.com>
2025-06-29 16:21:39 +0100

    changes to shell.nix so i don have to source .bashrc

--- dev/shell.nix
+++ dev/shell.nix
@@ -26,6 +26,12 @@ mkShell {
   ];
 
   shellHook = ''
+    # Some setup for stuff i like
+    eval "$(zoxide init bash)"
+    alias bashrc="nvim /home/asad/.bashrc"
+    alias cct="mvn clean compile test"
+
+    #Export some stuff
     export EDITOR=nvim
     export JAVA_HOME=${openjdk21}/lib/openjdk
   '';
